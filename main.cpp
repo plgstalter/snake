392545219741ef3762eae7e85f137d4e7c74081c
plgstalter <pierrelouisgstalter67@gmail.com>
2020-12-09 16:10:27 +0100

    changements esthétiques

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 #include "food.h"
 
 
+// Première partie : Setup avant le début de la partie
+
 void backgroundSetup(const int nx, const int ny, int background[]){
   // on va ajouter les '#' sur les bords
   // or dans `display.cpp`, les '#' correspondent à '1'
@@ -21,6 +23,20 @@ void backgroundSetup(const int nx, const int ny, int background[]){
     background[j*nx+nx-1] = 1;
   }
 }
+
+void setupSnake(int snake[], int len, int nx, int ny){
+  // le + 1 est pour éviter d'être sur la frontière ; idem pour le -2 (dans l'autre sens)
+  snake[0] = 8;
+  snake[SNAKE_LEN] = ny-10;
+  for (int i=1; i < len; i++) {
+    // on initialise un serpent horizontal
+    snake[i] = snake[i-1] - 1;
+    snake[SNAKE_LEN+i] = snake[SNAKE_LEN+i-1]; 
+  }
+}
+
+
+// 2e partie : ajouter et supprimer le serpent
 
 void add_snake(int* snake, int bg[], int snl, const int nx, const int ny){
   //on ajoute le snake sur le background ; un '1' affiche un '#'
@@ -35,6 +51,9 @@ void remove_snake(int* snake, int bg[], int snl, const int nx, const int ny){
     bg[snake[i] + nx*snake[SNAKE_LEN+i]] = -1;
   }
 }
+
+
+// 3e partie : faire évoluer le snake
 
 void snakegrowth(int* snake, int snl, int dxdy[]) {
   for (int i=0; i<snl; i++) {
@@ -65,49 +84,6 @@ void snake_movement(char key, int dxdy[]){
   }
 }
 
-bool verifyBorder(int snake[], int nx, int ny, int snl){
-
-  // 1ere partie : vérifier que l'on évite les bords
-  // on retrouve dxdy en faisant la diff entre snake[snl] et snake[snl-1]
-  int diff[2] = {snake[snl] - snake[snl-1], snake[snl+SNAKE_LEN] - snake[snl-1+SNAKE_LEN]};
-  if (diff[0]== 1 && snake[snl-1] == nx-1) {
-    return false;
-  }
-  if (diff[0]== -1 && snake[snl-1] == 0) {
-    return false;
-  }
-  if (diff[1]== 1 && snake[snl+SNAKE_LEN-1] == ny-1) {
-    return false;
-  }
-  if (diff[1]== -1 && snake[snl+SNAKE_LEN-1] == 0) {
-    return false;
-  }
-
-  // 2nde partie : vérifier que le serpent ne se mord pas la queue
-  // idée : on regarde si la dernière case + diff fonce sur le serpent (i.e. vaut une des autres cases)
-  int test_1 = snake[snl-1] + diff[0];
-  int test_2 = snake[snl-1+SNAKE_LEN] + diff[1];
-  for (int i=0; i<snl-1; i++) {
-    if (snake[i] == test_1 && snake[i+SNAKE_LEN] == test_2) {
-      return false;
-    }
-  }
-  
-  // si aucun problème, on est bon
-  return true;
-}
-
-void setupSnake(int snake[], int len, int nx, int ny){
-  // le + 1 est pour éviter d'être sur la frontière ; idem pour le -2 (dans l'autre sens)
-  snake[0] = 8;
-  snake[SNAKE_LEN] = ny-10;
-  for (int i=1; i < len; i++) {
-    // on initialise un serpent horizontal
-    snake[i] = snake[i-1] - 1;
-    snake[SNAKE_LEN+i] = snake[SNAKE_LEN+i-1]; 
-  }
-}
-
 void update_snake_coordinates(int snake[], int snl, int dxdy[]){
   for (int i=0;i<(snl - 1);i++) {
     snake[i] = snake[i+1];
@@ -116,6 +92,44 @@ void update_snake_coordinates(int snake[], int snl, int dxdy[]){
   snake[snl-1] = snake[snl - 1] + dxdy[0];
   snake[SNAKE_LEN+snl-1] = snake[SNAKE_LEN+snl - 1] + dxdy[1];
 }
+
+
+// 4e partie : game over
+
+bool snake_eats_itself(int* snake, int nx, int ny, int snl, int dxdy[]){
+
+}
+
+bool verifyBorder(int snake[], int nx, int ny, int snl, int dxdy[]){
+
+  // // 1ere partie : vérifier que l'on évite les bords
+  // if (dxdy[0]== 1 && snake[snl-1] == nx-1) {
+  //   return false;
+  // }
+  // if (dxdy[0]== -1 && snake[snl-1] == 0) {
+  //   return false;
+  // }
+  // if (dxdy[1]== 1 && snake[snl+SNAKE_LEN-1] == ny-1) {
+  //   return false;
+  // }
+  // if (dxdy[1]== -1 && snake[snl+SNAKE_LEN-1] == 0) {
+  //   return false;
+  // }
+
+  // // 2nde partie : vérifier que le serpent ne se mord pas la queue
+  // // idée : on regarde si la dernière case + dxdy fonce sur le serpent (i.e. vaut une des autres cases)
+  // int test_1 = snake[snl-1] + dxdy[0];
+  // int test_2 = snake[snl-1+SNAKE_LEN] + dxdy[1];
+  // for (int i=0; i<snl-1; i++) {
+  //   if (snake[i] == test_1 && snake[i+SNAKE_LEN] == test_2) {
+  //     return false;
+  //   }
+  // si aucun problème, on est bon
+  return true;
+}
+
+
+// Dernière partie : la partie elle-même :)
 
 void startGame(const int& lap, const int& nx, const int& ny, int& snl, int* snake, int* bg){
     char key;
@@ -134,8 +148,9 @@ void startGame(const int& lap, const int& nx, const int& ny, int& snl, int* snak
         add_snake(snake, bg, snl, nx, ny );
         printFrame(nx, ny, bg);
         remove_snake(snake, bg, snl, nx, ny);
-        bool out =  verifyBorder(snake, nx, ny, snl);
-        if( out == false){
+        bool out_1 = verifyBorder(snake, nx, ny, snl, dxdy);
+        bool out_2 = snake_eats_itself(snake, nx, ny, snl, dxdy);
+        if (!(out_1 && out_2)) {
             std::cerr << "" << std::endl;
             exit(1);
         }
